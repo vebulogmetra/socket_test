d1e2bc477cedf00d097de5c1fd63804922e5fc22
Artem <domitem@rambler.ru>
2021-09-19 02:50:45 +0300

    + INET sockets ex

---
+++ i_server.py
@@ -0,0 +1,19 @@
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 8001
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print("Connection address: {}".format(addr))
+while True:
+    data = conn.recv(BUFFER_SIZE)
+    if not data:
+        break
+    print("received data: {}".format(data))
+    conn.send(data)
+conn.close()
